9950bc0ad15701236bff13b4f1903d702638f16b
archonward <arthurteng@outlook.com>
2026-01-23 14:10:40 +0800

    Level-0: rename chatbot and add greet

---
+++ src/main/java/Nebula.java
@@ -0,0 +1,11 @@
+public class Nebula {
+    public static void main(String[] args) {
+        //String line = "____________________________________________________________";
+
+        System.out.println(" Hello! I'm Nebula");
+        System.out.println(" What can I do for you?");
+
+        System.out.println(" Bye. Hope to see you again soon!");
+
+    }
+}
