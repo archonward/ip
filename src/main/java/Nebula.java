1fc66d71556c74408c1fb0cdccfd1a2544e5a025
archonward <arthurteng@outlook.com>
2026-01-23 14:29:39 +0800

    Level 1. Echo

--- src/main/java/Nebula.java
+++ src/main/java/Nebula.java
@@ -1,11 +1,23 @@
+import java.util.Scanner;
+
 public class Nebula {
     public static void main(String[] args) {
-        //String line = "____________________________________________________________";
+        Scanner sc = new Scanner(System.in);
 
         System.out.println(" Hello! I'm Nebula");
         System.out.println(" What can I do for you?");
 
-        System.out.println(" Bye. Hope to see you again soon!");
+        while (true) {
+            String input = sc.nextLine().trim();
 
+            if (input.equals("bye")) {
+                System.out.println(" Bye. Hope to see you again soon!");
+                break;
+            }
+
+            System.out.println(input);
+        }
+
+        sc.close();
     }
 }
