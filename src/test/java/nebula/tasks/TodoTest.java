25b7fc4ec477331de1e758e84ab7a0c5edd087bb
archonward <arthurteng@outlook.com>
2026-02-01 15:17:14 +0800

    Added the 2 JUnit tests

---
+++ src/test/java/nebula/tasks/TodoTest.java
@@ -0,0 +1,41 @@
+package nebula.tasks;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TodoTest {
+
+    @Test
+    void newTodo_isNotDoneByDefault() {
+        Todo todo = new Todo("Read book");
+        assertFalse(todo.isDone());
+    }
+
+    @Test
+    void markDone_changesStateToDone() {
+        Todo todo = new Todo("Read book");
+        assertFalse(todo.isDone());
+
+        todo.markDone();
+        assertTrue(todo.isDone());
+    }
+
+    @Test
+    void markNotDone_changesStateBackToNotDone() {
+        Todo todo = new Todo("Read book");
+        todo.markDone();
+        assertTrue(todo.isDone());
+
+        todo.markNotDone();
+        assertFalse(todo.isDone());
+    }
+
+    @Test
+    void toString_includesCorrectPrefixAndStatus() {
+        Todo todo = new Todo("Read book");
+        assertEquals("[T][ ] Read book", todo.toString());
+
+        todo.markDone();
+        assertEquals("[T][X] Read book", todo.toString());
+    }
+}
